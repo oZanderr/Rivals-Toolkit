cbc17af56d10d734856a9a44c79cf73032096582
oZanderr <74891210+oZanderr@users.noreply.github.com>
2026-02-25 20:22:05 -0800

    Detect LoadingBay install

--- src-tauri/src/detect.rs
+++ src-tauri/src/detect.rs
@@ -1,4 +1,5 @@
 mod epic;
+mod loading_bay;
 mod registry;
 mod steam;
 
@@ -7,6 +8,7 @@ use std::path::PathBuf;
 use serde::{Deserialize, Serialize};
 
 use epic::find_epic_install;
+use loading_bay::find_loading_bay_install;
 use steam::find_steam_install;
 
 #[derive(Serialize, Deserialize)]
@@ -34,4 +36,5 @@ impl InstallInfo {
 pub(crate) fn detect_game_install() -> Option<InstallInfo> {
     None.or_else(|| find_steam_install().map(|p| InstallInfo::new(p, InstallSource::Steam)))
         .or_else(|| find_epic_install().map(|p| InstallInfo::new(p, InstallSource::Epic)))
+        .or_else(|| find_loading_bay_install().map(|p| InstallInfo::new(p, InstallSource::LoadingBay)))
 }
